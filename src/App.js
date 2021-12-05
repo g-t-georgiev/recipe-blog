7731fa1b562d1cc126bfd0009cd37466d819ece3
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 22:36:21 +0200

    Added routes to home, login and register pages; imported header with navigation.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
 function App() {
-  return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/auth/login" element={<Login />} />
+				<Route path="/auth/register" element={<Register />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
