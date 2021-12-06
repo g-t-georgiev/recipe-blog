57eb1badbe9cd53b2e171c7932994bc99a33d441
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-06 21:02:50 +0200

    Changed import paths for login and register to point auth folder directory and imported site wrapper and main content css styles from app css module.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 
+import cssClasses from './App.module.css';
+
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import Login from './components/Auth/Login/Login';
+import Register from './components/Auth/Register/Register';
 
 function App() {
 	return (
-		<>
+		<div className={cssClasses['layout-wrapper']}>
 			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/auth/login" element={<Login />} />
-				<Route path="/auth/register" element={<Register />} />
-			</Routes>
-		</>
+			<main className={cssClasses['main-content']}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/auth/login" element={<Login />} />
+					<Route path="/auth/register" element={<Register />} />
+				</Routes>
+			</main>
+		</div>
 	);
 }
 
