46537ac4d4055aa7f3fee804f5a2fa9710342058
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-06 21:00:02 +0200

    Imported styles from header css module.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import { NavLink } from "react-router-dom";
 
+import cssClasses from './Header.module.css';
+
+const setActiveLinkStyles = function ({ isActive }) {
+    const linkStyles = ['site-navigation-link', 'active'];
+    return isActive 
+        ? `${cssClasses[linkStyles[0]]} ${cssClasses[linkStyles[1]]}`
+        : `${cssClasses[linkStyles[0]]}`;
+}
+
 function Header(props) {
     return (
-        <header>
-            <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/auth/login">Login</NavLink>
-                <NavLink to="/auth/register">Register</NavLink>
+        <header className={cssClasses['site-header']}>
+            <h2 className={cssClasses['site-header-title']}>Recipe Blog</h2>
+            <nav className={cssClasses['site-navigation']}>
+                <NavLink className={setActiveLinkStyles} to="/">Home</NavLink>
+                <NavLink className={setActiveLinkStyles} to="/auth/login">Login</NavLink>
+                <NavLink className={setActiveLinkStyles} to="/auth/register">Register</NavLink>
             </nav>
         </header>
     );
