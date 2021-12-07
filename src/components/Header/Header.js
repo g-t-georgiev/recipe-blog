e64cea73564c16f0134e08ebebb2b401013e5bde
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-07 18:20:36 +0200

    Imported styles and changed class name attributes to strings.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,18 @@
 import { NavLink } from "react-router-dom";
 
-import cssClasses from './Header.module.css';
+import './Header.css';
 
 const setActiveLinkStyles = function ({ isActive }) {
-    const linkStyles = ['site-navigation-link', 'active'];
     return isActive 
-        ? `${cssClasses[linkStyles[0]]} ${cssClasses[linkStyles[1]]}`
-        : `${cssClasses[linkStyles[0]]}`;
+        ? 'site-navigation-link link-active'
+        : 'site-navigation-link';
 }
 
 function Header(props) {
     return (
-        <header className={cssClasses['site-header']}>
-            <h2 className={cssClasses['site-header-title']}>Recipe Blog</h2>
-            <nav className={cssClasses['site-navigation']}>
+        <header className="site-header">
+            <h2 className="site-header-title">Recipe Blog</h2>
+            <nav className="site-navigation">
                 <NavLink className={setActiveLinkStyles} to="/">Home</NavLink>
                 <NavLink className={setActiveLinkStyles} to="/auth/login">Login</NavLink>
                 <NavLink className={setActiveLinkStyles} to="/auth/register">Register</NavLink>
