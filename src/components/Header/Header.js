907ca0e3293dea2e1f732659482635205569711c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-08 21:59:24 +0200

    Added state for opened and closed menu, toggle navigation handler, created link group to be shown/displayed depending on state for small with screens, otherwise show always.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,36 @@
+import { useState } from 'react';
 import { NavLink } from "react-router-dom";
 
 import './Header.css';
 
+import ToggleNavButton from "./ToggleNavButton/ToggleNavButton";
+
 const setActiveLinkStyles = function ({ isActive }) {
-    return isActive 
+    return isActive
         ? 'site-navigation-link link-active'
         : 'site-navigation-link';
 }
 
-function Header(props) {
+function Header() {
+    const [navState, setNavState] = useState({ opened: false });
+
+    const toggleNavState = function () {
+        setNavState({ opened: !navState.opened });
+    }
+
     return (
         <header className="site-header">
-            <h2 className="site-header-title">Recipe Blog</h2>
+            <h2 className="site-header-title">
+                Food Blog
+            </h2>
             <nav className="site-navigation">
-                <NavLink className={setActiveLinkStyles} to="/">Home</NavLink>
-                <NavLink className={setActiveLinkStyles} to="/auth/login">Login</NavLink>
-                <NavLink className={setActiveLinkStyles} to="/auth/register">Register</NavLink>
+                <ToggleNavButton toggleNavHandler={toggleNavState} />
+                <section className={`site-navigation-links ${navState.opened ? 'opened' : 'closed'}`} onClick={toggleNavState}>
+                    <NavLink className={setActiveLinkStyles} to="/about">About</NavLink>
+                    <NavLink className={setActiveLinkStyles} to="/">Recipes</NavLink>
+                    <NavLink className={setActiveLinkStyles} to="/auth/login">Login</NavLink>
+                    <NavLink className={setActiveLinkStyles} to="/auth/register">Register</NavLink>
+                </section>
             </nav>
         </header>
     );
