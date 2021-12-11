018c3981766797fb1ce2658e5c6a8cd3acd21c4c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-11 21:06:47 +0200

    Added some validation conditions, like required input, email format validation and password length validation.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -5,6 +5,8 @@ import FormFooter from '../../shared/form/form-footer/FormFooter';
 import FormInput from '../../shared/form/form-input/FormInput';
 import FormButton from '../../shared/form/form-button/FormButton';
 
+import { emailRegexPattern } from '../../../constants';
+
 function Login() {
 
     const submitHandler = function () {
@@ -14,8 +16,8 @@ function Login() {
     return (
         <section>
             <Form formTitle="Sign in to your account" formSubmitAction={submitHandler}>
-                <FormInput type="email" name="email" placeholder="Enter your email.." />
-                <FormInput type="password" name="password" placeholder="Enter password.." />
+                <FormInput type="email" name="email" placeholder="Enter your email.." validationConditions={{ isRequired: { value: true, message: 'Email is required' }, isEmail: { value: emailRegexPattern, message: 'Invalid email address' }}} />
+                <FormInput type="password" name="password" placeholder="Enter password.." validationConditions={{ isRequired: { value: true, message: 'Password is required.' }, minLength: { value: 6, message: 'Password should be at least 6 characters long.' }}} />
                 <FormButton>Sign in</FormButton>
                 <FormFooter>
                     Not a member yet? &nbsp;
