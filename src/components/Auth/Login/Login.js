c16145f1c2015350cce57d1c63025c068416b746
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-12 17:29:29 +0200

    Removed props passing for validation conditions.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,23 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import Form from '../../shared/form/Form';
 import FormFooter from '../../shared/form/form-footer/FormFooter';
 import FormInput from '../../shared/form/form-input/FormInput';
 import FormButton from '../../shared/form/form-button/FormButton';
 
-import { emailRegexPattern } from '../../../constants';
-
 function Login() {
-
-    const submitHandler = function () {
-        console.log('Submitted');
-    }
 
     return (
         <section>
-            <Form formTitle="Sign in to your account" formSubmitAction={submitHandler}>
-                <FormInput type="email" name="email" placeholder="Enter your email.." validationConditions={{ isRequired: { value: true, message: 'Email is required' }, isEmail: { value: emailRegexPattern, message: 'Invalid email address' }}} />
-                <FormInput type="password" name="password" placeholder="Enter password.." validationConditions={{ isRequired: { value: true, message: 'Password is required.' }, minLength: { value: 6, message: 'Password should be at least 6 characters long.' }}} />
+            <Form name="loginForm" title="Sign in to your account">
+                <FormInput type="email" name="email" placeholder="Enter your email.." />
+                <FormInput type="password" name="password" placeholder="Enter password.." />
                 <FormButton>Sign in</FormButton>
                 <FormFooter>
                     Not a member yet? &nbsp;
