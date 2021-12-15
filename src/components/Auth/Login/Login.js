abb0d4948271fd1126aa85c48d3efe1440005865
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-15 22:46:34 +0200

    Pass form element attributes and data as property of form component.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,23 +1,45 @@
-import { Link } from 'react-router-dom';
-
 import Form from '../../shared/form/Form';
-import FormFooter from '../../shared/form/form-footer/FormFooter';
-import FormInput from '../../shared/form/form-input/FormInput';
-import FormButton from '../../shared/form/form-button/FormButton';
+
+const elements = {
+    inputs: [
+        {
+            id: 1,
+            type: 'email',
+            name: 'email',
+            placeholder: 'Enter your email..',
+            error: 'Please provide valid email format.',
+            pattern: '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
+            required: true
+        },
+        {
+            id: 2,
+            type: 'password',
+            name: 'password',
+            placeholder: 'Enter password..',
+            error: 'Password length should be between 6 and 15 characters and should contain at least one letter and/or digit and optionally the following special symbols: @$!%*#?&',
+            pattern: '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{6,15}$',
+            required: true
+        }
+    ],
+    button: {
+        text: 'Sign in',
+        type: 'submit'
+    },
+    footer: {
+        span: {
+            text: 'Not a member yer?'
+        },
+        link: {
+            text: 'Sign up.',
+            to: '/auth/register'
+        }
+    }
+};
 
 function Login() {
-
     return (
         <section>
-            <Form name="loginForm" title="Sign in to your account">
-                <FormInput type="email" name="email" placeholder="Enter your email.." />
-                <FormInput type="password" name="password" placeholder="Enter password.." />
-                <FormButton>Sign in</FormButton>
-                <FormFooter>
-                    Not a member yet? &nbsp;
-                    <Link to="/auth/register">Sign up.</Link>
-                </FormFooter>
-            </Form>
+            <Form name="loginForm" title="Sign in to your account" elements={elements} />
         </section>
     );
 }
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,26 +1,63 @@
-import { Link } from 'react-router-dom';
-
 import Form from '../../shared/form/Form';
-import FormFooter from '../../shared/form/form-footer/FormFooter';
-import FormInput from '../../shared/form/form-input/FormInput';
-import FormButton from '../../shared/form/form-button/FormButton';
+
+const elements = {
+    inputs: [
+        {
+            id: 1,
+            type: 'text',
+            name: 'username',
+            placeholder: 'Enter username..',
+            error: 'Username should be between 4 and 15 characters long and could consist of latin letters, digits, hyphens, underscores and dots.',
+            pattern: '',
+            required: true
+        },
+        {
+            id: 2,
+            type: 'email',
+            name: 'email',
+            placeholder: 'Enter your email..',
+            error: 'Please provide valid email format.',
+            pattern: '',
+            required: true
+        },
+        {
+            id: 3,
+            type: 'password',
+            name: 'password',
+            placeholder: 'Enter password..',
+            error: 'Password should be between 6 and 15 characters long.',
+            pattern: '',
+            required: true
+        },
+        {
+            id: 4,
+            type: 'password',
+            name: 'confirmPassword',
+            placeholder: 'Repeat password..',
+            error: 'Passwords do not match.',
+            pattern: '',
+            required: true
+        }
+    ],
+    button: {
+        text: 'Sign up',
+        type: 'submit'
+    },
+    footer: {
+        span: {
+            text: 'Already have an account?'
+        },
+        link: {
+            text: 'Sign in.',
+            to: '/auth/login'
+        }
+    }
+};
 
 function Register() {
-
-
     return (
         <section>
-            <Form name="registerForm" title="Create an account">
-                <FormInput name="username" placeholder="Enter username.." />
-                <FormInput type="email" name="email" placeholder="Enter email address.." />
-                <FormInput type="password" name="password" placeholder="Enter password.." />
-                <FormInput type="password" name="confirmPassword" placeholder="Repeat password.." />
-                <FormButton>Sign up</FormButton>
-                <FormFooter>
-                    Already a member? &nbsp;
-                    <Link to="/auth/login">Sign in.</Link>
-                </FormFooter>
-            </Form>
+            <Form name="registerForm" title="Create an account" elements={elements} />
         </section>
     );
 }
