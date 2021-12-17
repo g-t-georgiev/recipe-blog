8ee7694b73a0b2ba5be6e767d063d55e79f6ea61
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-17 19:05:26 +0200

    Display error message box only if error is present.

--- src/components/shared/form-input/FormInput.js
+++ src/components/shared/form-input/FormInput.js
@@ -12,8 +12,8 @@ function FormInput({ error, ...props }) {
     return (
         <div className="form-row">
             <input className="form-input" {...props} />
-            <i className="info-icon">&#33;</i>
-            <span className="error-message">{error}</span>
+            <i className="info-icon">@</i>
+            {error && <span className="error-message">{error}</span>}
         </div>
     );
 }
