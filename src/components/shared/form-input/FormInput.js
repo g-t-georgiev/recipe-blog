3ffad32e7ba82a881bc7a805a3b082633639fb23
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-17 19:06:03 +0200

    Removed hook imports.

--- src/components/shared/form-input/FormInput.js
+++ src/components/shared/form-input/FormInput.js
@@ -1,14 +1,6 @@
-// import { useCallback } from 'react';
-// import { useFormContext } from '../form/FormContext';
 import './FormInput.css';
 
 function FormInput({ error, ...props }) {
-    // const [ formStatus, setFormStatus ] = useFormContext();
-
-    // const changeHandler = useCallback(function (e) {
-        
-    // }, []);
-
     return (
         <div className="form-row">
             <input className="form-input" {...props} />
