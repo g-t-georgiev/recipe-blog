8826e43364b24268a04e2d4e495ba5014eef3297
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-17 20:25:17 +0200

    Removed external elements schema object and added it to form component.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,37 +1,9 @@
 import Form from '../../shared/form/Form';
 
-const elements = {
-    inputs: [
-        {
-            name: 'email',
-            required: true
-        },
-        {
-            name: 'password',
-            required: true
-        }
-    ],
-    button: [
-        {
-            text: 'Sign in',
-            type: 'submit'
-        }
-    ],
-    footer: {
-        span: {
-            text: 'Not a member yer?'
-        },
-        link: {
-            text: 'Sign up.',
-            to: '/auth/register'
-        }
-    }
-};
-
 function Login() {
     return (
         <section>
-            <Form name="loginForm" title="Sign in to your account" elements={elements} />
+            <Form name="loginForm" title="Sign in to your account" />
         </section>
     );
 }
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,41 +1,9 @@
 import Form from '../../shared/form/Form';
 
-const elements = {
-    inputs: [
-        {
-            name: 'username',
-            required: true
-        },
-        {
-            name: 'email',
-            required: true
-        },
-        {
-            name: 'password',
-            required: true
-        }
-    ],
-    buttons: [
-        {
-            text: 'Sign up',
-            type: 'submit'
-        }
-    ],
-    footer: {
-        span: {
-            text: 'Already have an account?'
-        },
-        link: {
-            text: 'Sign in.',
-            to: '/auth/login'
-        }
-    }
-};
-
 function Register() {
     return (
         <section>
-            <Form name="registerForm" title="Create an account" elements={elements} />
+            <Form name="registerForm" title="Create an account" />
         </section>
     );
 }
