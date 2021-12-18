75466f7bd4f9af6674ccd27fdee7c42d67a5b829
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-18 17:46:45 +0200

    Added error handling state for both mobile and desktop views.

--- src/components/shared/form-input/FormInput.js
+++ src/components/shared/form-input/FormInput.js
@@ -1,16 +1,25 @@
+import { useState, useCallback } from 'react';
 import './FormInput.css';
 
 function FormInput({ error, ...props }) {
+    const [ value, setValue ] = useState('');
+    const changeHandler = useCallback(function (e) {
+        setValue(e.target.value);
+    }, [setValue]);
+
     return (
         <div className="form-row">
-            <label htmlFor={props.id}>
-                <input className="form-input" {...props} />
-                <span id="label-text">
-                    {props.placeholder}
-                </span>
-                <i className="info-icon">{error ? '\u2716' : '\u2713'}</i>
-                <span className="error-message">{error || 'Looks great.'}</span>
-            </label>
+            <div className={`input-field ${error ? 'invalid' : 'valid'}`}>
+                <label htmlFor={props.id}>
+                    <input className="form-input" {...props} onChange={changeHandler} />
+                    <span id="label-text">
+                        {props.placeholder}
+                    </span>
+                    <i className="info-icon">{error ? '\u2716' : '\u2713'}</i>
+                    <span className="error-message">{error || 'Looks great.'}</span>
+                </label>
+            </div>
+            {(value && error) && <span className="error-message">{error}</span>}
         </div>
     );
 }
