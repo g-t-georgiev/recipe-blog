4f33de970f790a20511e403cd12c215f7f3241c9
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-19 19:27:29 +0200

    Refactored form component by passing error state to children as context and added custom hook for getting form context to use in context provider's children components.

--- src/components/shared/form/Form.js
+++ src/components/shared/form/Form.js
@@ -1,156 +1,32 @@
-import { useState, useCallback } from "react";
+import { createContext, useContext, useState, useCallback, useMemo } from "react";
 import './Form.css';
-import FormInput from "../form-input/FormInput";
-import FormButton from "../form-button/FormButton";
-import FormFooter from "../form-footer/FormFooter";
 
-const usernamePattern = '^(?=[a-zA-Z0-9._\\-]{4,15}$)(?!.*[_.\\-]{2})[^_.\\-].*[^_.\\-]$';
-const emailPattern = '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])';
-const passwordPattern = '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,15}$';
+// const usernamePattern = '^(?=[a-zA-Z0-9._\\-]{4,15}$)(?!.*[_.\\-]{2})[^_.\\-].*[^_.\\-]$';
+// const emailPattern = '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])';
+// const passwordPattern = '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,15}$';
 
-const fieldSchema = {
-    create(key, error, { type, name, id, placeholder, pattern, required }) {
-        return {
-            key,
-            error,
-            props: {
-                type,
-                name,
-                id,
-                placeholder,
-                pattern,
-                required
-            }
-        }
-    }
-}
+export const FormContext = createContext(null);
 
-const initialFormState = {
-    valid: false,
-    name: '',
-    elements: {
-        fields: {
-            loginForm: {
-                email: fieldSchema.create(
-                    1,
-                    null,
-                    {
-                        type: 'email',
-                        name: 'email',
-                        id: 'email',
-                        placeholder: 'Email',
-                        pattern: emailPattern,
-                        required: true
-                    }
-                ),
-                password: fieldSchema.create(
-                    2,
-                    null,
-                    {
-                        type: 'password',
-                        name: 'password',
-                        id: 'password',
-                        placeholder: 'Password',
-                        pattern: passwordPattern,
-                        required: true
-                    }
-                )
-            },
-            registerForm: {
-                username: fieldSchema.create(
-                    1,
-                    null,
-                    {
-                        type: 'username',
-                        name: 'username',
-                        id: 'username',
-                        placeholder: 'Username',
-                        pattern: usernamePattern,
-                        required: true
-                    }
-                ),
-                email: fieldSchema.create(
-                    2,
-                    null,
-                    {
-                        type: 'email',
-                        name: 'email',
-                        id: 'email',
-                        placeholder: 'Email',
-                        pattern: emailPattern,
-                        required: true
-                    }
-                ),
-                password: fieldSchema.create(
-                    3,
-                    null,
-                    {
-                        type: 'password',
-                        name: 'password',
-                        id: 'password',
-                        placeholder: 'Password',
-                        pattern: passwordPattern,
-                        required: true
-                    }
-                )
-            }
-        },
-        buttons: {
-            registerForm: {
-                submit: {
-                    text: 'Sign up',
-                    type: 'submit'
-                }
-            },
-            loginForm: {
-                submit: {
-                    text: 'Sign in',
-                    type: 'submit'
-                }
-            }
-        },
-        footer: {
-            registerForm: {
-                span: {
-                    text: 'Already have an account?'
-                },
-                link: {
-                    text: 'Sign in.',
-                    to: '/auth/login'
-                }
-            },
-            loginForm: {
-                span: {
-                    text: 'Not a member yer?'
-                },
-                link: {
-                    text: 'Sign up.',
-                    to: '/auth/register'
-                }
-            }
-        }
-    }
-
+export const useFormContext = function () {
+    return useContext(FormContext);
 };
 
-function Form({ name, title = 'Fill in the form below' }) {
-    const [formState, setFormState] = useState(function () {
+function Form({ name, title = 'Fill in the form below', children }) {
+    const [ error, setError ] = useState(null);
+
+    const formError = useMemo(function () {
         return {
-            name,
-            valid: false,
-            elements: {
-                fields: {
-                    ...initialFormState.elements.fields[name]
-                },
-                buttons: {
-                    ...initialFormState.elements.buttons[name]
-                },
-                footer: {
-                    ...initialFormState.elements.footer[name]
-                }
+            get() {
+                return error;
+            },
+            set(value = '') {
+                setError(value);
+            },
+            has() {
+                return Boolean(error);
             }
         };
-    });
+    }, [error, setError]);
 
     const submitHandler = useCallback(function (e) {
         e.preventDefault();
@@ -161,48 +37,16 @@ function Form({ name, title = 'Fill in the form below' }) {
 
         console.log(formFieldData);
 
-        console.log('Registered successfully.');
-    }, []);
-
-    const changeHandler = useCallback(function (e) {
-        const fieldName = e.target.name;
-        const fieldValue = e.target.value;
-        const fieldPattern = new RegExp(e.target.pattern);
-        const isValid = fieldPattern.test(fieldValue);
-
-        let message = fieldName === 'username' 
-            ? 'Username should be between 4 and 15 characters and can contain letters and digits, as well as hyphen, dot or underscore as a word separator.'
-            : fieldName === 'email'
-            ? 'Email should be in the format: <name>@<host>.'
-            : fieldName === 'password'
-            ? 'Password should be between 6 and 15 characters long and should contain one of the following: a letter, a digit and one of the allowed special symbols: @$!%*#?&'
-            : 'Unknown error.';
-
-        setFormState(state => ({
-            ...state,
-            valid: isValid,
-            elements: {
-                ...state.elements,
-                fields: {
-                    ...state.elements.fields,
-                    [fieldName]: {
-                        ...state.elements.fields[fieldName],
-                        error: isValid
-                            ? null
-                            : message
-                    }
-                }
-            }
-        }));
+        console.log('Form submitted successfully.');
     }, []);
 
     return (
-        <form className="form" name={name} autoComplete="off" onSubmit={submitHandler} onChange={changeHandler}>
-            <legend className="form-title">{title}</legend>
-            {Object.values(formState.elements.fields).map(field => <FormInput key={field.key} error={field.error} {...field.props} />)}
-            {Object.values(formState.elements.buttons).map((button, i) => <FormButton key={i + 1} disabled={!formState.valid} {...button} />)}
-            {<FormFooter {...formState.elements.footer} />}
-        </form>
+        <FormContext.Provider value={formError}>
+            <form className="form" name={name} autoComplete="off" onSubmit={submitHandler}>
+                <legend className="form-title">{title}</legend>
+                {children}
+            </form>
+        </FormContext.Provider>
     );
 }
 
