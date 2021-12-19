4b103be2818aa5f4c0a175a37e255c38e8301aa3
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-19 19:25:33 +0200

    Refactored form component presentation logic.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,9 +1,17 @@
 import Form from '../../shared/form/Form';
+import FormInput from '../../shared/form-input/FormInput';
+import FormButton from '../../shared/form-button/FormButton';
+import FormFooter from '../../shared/form-footer/FormFooter';
 
 function Login() {
     return (
         <section>
-            <Form name="loginForm" title="Sign in to your account" />
+            <Form name="loginForm" title="Sign in to your account">
+                <FormInput type="email" name="email" id="email" placeholder="Email" />
+                <FormInput type="password" name="password" id="password" placeholder="Password" />
+                <FormButton text="Sign in" />
+                <FormFooter span={{ text: 'Not a member yet?' }} link={{ text: 'Sign up.', to: '/auth/register' }} />
+            </Form>
         </section>
     );
 }
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,9 +1,18 @@
 import Form from '../../shared/form/Form';
+import FormInput from '../../shared/form-input/FormInput';
+import FormButton from '../../shared/form-button/FormButton';
+import FormFooter from '../../shared/form-footer/FormFooter';
 
 function Register() {
     return (
         <section>
-            <Form name="registerForm" title="Create an account" />
+            <Form name="registerForm" title="Create an account">
+                <FormInput type="username" name="username" id="username" placeholder="Username" />
+                <FormInput type="email" name="email" id="email" placeholder="Email" />
+                <FormInput type="password" name="password" id="password" placeholder="Password" />
+                <FormButton text="Sign up" />
+                <FormFooter span={{ text: 'Already have an account?' }} link={{ text: 'Sign in.', to: '/auth/login' }} />
+            </Form>
         </section>
     );
 }
