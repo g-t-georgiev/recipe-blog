6f19a248dcf327202526fb1816eaf53465a2336a
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-19 21:17:39 +0200

    Added validation for username in change handler.

--- src/components/shared/form-input/FormInput.js
+++ src/components/shared/form-input/FormInput.js
@@ -23,22 +23,43 @@ function FormInput(props) {
     }, [error, setError]);
 
     const changeHandler = useCallback(function (e) {
+        const inputValue = e.currentTarget.value;
+        let message = '';
+
+        if (props.name === 'username') {
+            if (inputValue.length < 4) {
+                message = 'Username should be at least 4 characters long.';
+            }
+
+            if (inputValue.length > 15) {
+                message = 'Username should be 15 characters long at maximum.';
+            }
+
+            if (!/[a-z0-9_.\-]/gi.test(inputValue)) {
+                message = 'Username should contain only english letters, digits and a dot, hyphen or underscore as word separator.';
+            }
+
+            if (!/^(?![_.\-])[a-z0-9_.\-]+(?<![_.\-])$/i.test(inputValue)) {
+                message = 'Username should start and end with a letter or a digit only.';
+            }
+
+            message && inputError.set(message);
+        }
         setValue(e.target.value);
     }, [setValue]);
 
     return (
         <div className="form-row">
-            <div className={`input-field ${error ? 'invalid' : 'valid'}`}>
+            <div className={`input-field ${formError.has() || inputError.has() ? 'invalid' : 'valid'}`}>
                 <label htmlFor={props.id}>
                     <input className="form-input" {...props} onChange={changeHandler} />
                     <span id="label-text">
                         {props.placeholder}
                     </span>
-                    <i className="info-icon">{error ? '\u2716' : '\u2713'}</i>
-                    <span className="error-message">{error || 'Looks great.'}</span>
                 </label>
+                <i className="info-icon">{formError.has() || inputError.has() ? '\u2716' : '\u2713'}</i>
             </div>
-            {(value && error) && <span className="error-message">{error}</span>}
+            <span className="error-message">{formError.get() || inputError.get() || 'Looks great.'}</span>
         </div>
     );
 }
