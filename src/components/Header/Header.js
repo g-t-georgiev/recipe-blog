a31bd3d4346b8f02c8e8b8e0f7a137454f3bdb88
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-21 21:13:17 +0200

    Mocked public links navigation state.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 import './Header.css';
 
@@ -27,12 +27,31 @@ function Header() {
                 <ToggleNavButton toggleNavHandler={toggleNavState} />
                 <section className={`site-navigation-links ${navState.opened ? 'opened' : 'closed'}`} onClick={toggleNavState}>
                     <NavLink className={setActiveLinkStyles} to="/">Home</NavLink>
-                    <NavLink className={setActiveLinkStyles} to="/recipes">Recipes</NavLink>
-                    <NavLink className={setActiveLinkStyles} to="/users/login">Login</NavLink>
-                    <NavLink className={setActiveLinkStyles} to="/users/register">Register</NavLink>
+
+                    <span className="dropdown">
+                        <span className="dropdown-title">Recipes</span>
+                        <span className="dropdown-links">
+                            <Link className="site-navigation-link" to="/recipes">All</Link>
+                            <span className="dropdown-section-title">Categories</span>
+                            <Link className="site-navigation-link" to="/recipes?category=breakfast">Breakfast</Link>
+                            <Link className="site-navigation-link" to="/recipes?category=lunch">Lunch</Link>
+                            <Link className="site-navigation-link" to="/recipes?category=dinner">Dinner</Link>
+                            <Link className="site-navigation-link" to="/recipes?category=pasta">Pasta</Link>
+                            <Link className="site-navigation-link" to="/recipes?category=pizza">Pizza</Link>
+                            <Link className="site-navigation-link" to="/recipes?category=dessert">Dessert</Link>
+                        </span>
+                    </span>
+
+                    <span className="dropdown">
+                        <span className="dropdown-title">Profile</span>
+                        <span className="dropdown-links">
+                            <NavLink className={setActiveLinkStyles} to="/users/login">Login</NavLink>
+                            <NavLink className={setActiveLinkStyles} to="/users/register">Register</NavLink>
+                        </span>
+                    </span>
                 </section>
             </nav>
-        </header>
+        </header >
     );
 }
 
