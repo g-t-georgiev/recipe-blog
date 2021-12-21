2a2c2a137ae9eca8fbc3aeee00a967e731339915
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-21 16:19:25 +0200

    Imported authentication context provider and wrapped application header and routes.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from '../../contexts/AuthContext';
 
 import './App.css';
 
@@ -9,16 +10,18 @@ import Register from '../auth/register/Register';
 
 function App() {
 	return (
-		<div className="layout-wrapper">
-			<Header />
-			<main className="main-content">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/auth/login" element={<Login />} />
-					<Route path="/auth/register" element={<Register />} />
-				</Routes>
-			</main>
-		</div>
+		<AuthContextProvider>
+			<div className="layout-wrapper">
+				<Header />
+				<main className="main-content">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/auth/login" element={<Login />} />
+						<Route path="/auth/register" element={<Register />} />
+					</Routes>
+				</main>
+			</div>
+		</AuthContextProvider>
 	);
 }
 
