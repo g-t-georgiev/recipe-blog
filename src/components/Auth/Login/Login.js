a6419c2bbe289a821fae871dfb8c7487b8f4e16c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-21 21:15:23 +0200

    Changed link path to register page in footer.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -10,7 +10,7 @@ function Login() {
                 <FormInput type="email" name="email" id="email" placeholder="Email" />
                 <FormInput type="password" name="password" id="password" placeholder="Password" />
                 <FormButton text="Sign in" />
-                <FormFooter span={{ text: 'Not a member yet?' }} link={{ text: 'Sign up.', to: '/auth/register' }} />
+                <FormFooter span={{ text: 'Not a member yet?' }} link={{ text: 'Sign up.', to: '/users/register' }} />
             </Form>
         </section>
     );
