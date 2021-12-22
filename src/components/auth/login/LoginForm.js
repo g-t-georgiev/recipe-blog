9c3166bb9e4281412077016bae02188749ffd27a
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-22 16:59:22 +0200

    Created wrapper login form component for binding login request and response processing.

---
+++ src/components/auth/login/LoginForm.js
@@ -0,0 +1,23 @@
+import { useCallback } from 'react';
+import { useAuthContext } from '../../../contexts/AuthContext';
+import * as authService from '../../../services/authService';
+
+import Form from '../../shared/form/Form';
+import FormInput from '../../shared/form-input/FormInput';
+import FormButton from '../../shared/form-button/FormButton';
+import FormFooter from '../../shared/form-footer/FormFooter';
+
+function LoginForm() {
+    const loginHandler = useCallback(function () {}, []);
+
+    return (
+        <Form name="loginForm" title="Sign in to your account" submitAction={loginHandler}>
+            <FormInput type="email" name="email" id="email" placeholder="Email" />
+            <FormInput type="password" name="password" id="password" placeholder="Password" />
+            <FormButton text="Sign in" />
+            <FormFooter span={{ text: 'Not a member yet?' }} link={{ text: 'Sign up.', to: '/users/register' }} />
+        </Form>
+    )
+}
+
+export default LoginForm;
