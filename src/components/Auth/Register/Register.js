c15fb4ce1b0e73ffd91fb0e3e042ee08be1ce913
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-22 17:00:19 +0200

    Imported register form component.

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,18 +1,9 @@
-import Form from '../../shared/form/Form';
-import FormInput from '../../shared/form-input/FormInput';
-import FormButton from '../../shared/form-button/FormButton';
-import FormFooter from '../../shared/form-footer/FormFooter';
+import RegisterForm from './RegisterForm';
 
 function Register() {
     return (
         <section>
-            <Form name="registerForm" title="Create new account">
-                <FormInput type="text" name="username" id="username" placeholder="Username" />
-                <FormInput type="email" name="email" id="email" placeholder="Email" />
-                <FormInput type="password" name="password" id="password" placeholder="Password" />
-                <FormButton text="Sign up" />
-                <FormFooter span={{ text: 'Already have an account?' }} link={{ text: 'Sign in.', to: '/users/login' }} />
-            </Form>
+            <RegisterForm />
         </section>
     );
 }
