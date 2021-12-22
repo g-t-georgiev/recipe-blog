adbfa3274ae80ed1ceb00889a180eea60241b40c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-22 17:00:08 +0200

    Imported login form component.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,17 +1,9 @@
-import Form from '../../shared/form/Form';
-import FormInput from '../../shared/form-input/FormInput';
-import FormButton from '../../shared/form-button/FormButton';
-import FormFooter from '../../shared/form-footer/FormFooter';
+import LoginForm from "./LoginForm";
 
 function Login() {
     return (
         <section>
-            <Form name="loginForm" title="Sign in to your account">
-                <FormInput type="email" name="email" id="email" placeholder="Email" />
-                <FormInput type="password" name="password" id="password" placeholder="Password" />
-                <FormButton text="Sign in" />
-                <FormFooter span={{ text: 'Not a member yet?' }} link={{ text: 'Sign up.', to: '/users/register' }} />
-            </Form>
+            <LoginForm />
         </section>
     );
 }
