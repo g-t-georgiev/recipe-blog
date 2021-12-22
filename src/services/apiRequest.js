ecc9e4d722d04bcf63dddc266f803efe76d61415
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-22 15:28:56 +0200

    Added API requester methods.

---
+++ src/services/apiRequest.js
@@ -0,0 +1,51 @@
+import { apiUrl } from '../constants';
+
+const request = async function (method, path, payload, parseJSON) {
+    try {
+        method = method ?? 'get';
+
+        if (!path) {
+            throw new Error('Invalid argument path.');
+        }
+    
+        let options = {};
+    
+        if (['post', 'put', 'patch'].includes(method)) {
+            if (!payload) {
+                throw new Error('Invalid argument payload.');
+            }
+    
+            options.method = method;
+            options.headers = { 'content-type': 'application/json' };
+            options.body = JSON.parse(payload);
+        }
+    
+        let result = await fetch(apiUrl + path, options);
+    
+        if (parseJSON && result.ok) {
+            result = await result.json();
+        }
+    
+        return result;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+const api = {
+    get(path, parseJSON = true) {
+        return request('get', path, null, parseJSON);
+    },
+    post(path, payload, parseJSON = false) {
+        return request('post', path, payload, parseJSON);
+    },
+    put(path, payload, parseJSON = true) {
+        return request('put', path, payload, parseJSON);
+    },
+    delete(path, parseJSON = false) {
+        return request('delete', path, null, parseJSON);
+    }
+};
+
+export default api;
