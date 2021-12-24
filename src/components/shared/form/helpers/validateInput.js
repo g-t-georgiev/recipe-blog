3fd79d7d9b7da5873da9d0dbd4fe4278dc81e473
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 00:51:53 +0200

    Pulled out validation schema from module and simplified validation algorithm.

--- src/components/shared/form/helpers/validateInput.js
+++ src/components/shared/form/helpers/validateInput.js
@@ -1,40 +1,3 @@
-const inputRequirementsSchema = {
-    username: {
-        minLength: {
-            value: 4,
-            message: 'Username should be at least 4 characters long.'
-        },
-        maxLength: {
-            value: 20,
-            message: 'Username should be 20 characters long at maximum.',
-        },
-        match: {
-            value: /^(?![_.-\s])(?!.*[_.-\s]{2})[a-z0-9_.-]{4,20}(?<![_.-\s])$/i,
-            message: 'Username can contain letters, digits and a dot, underscore or a hyphen as a word separator.'
-        }
-    },
-    email: {
-        match: {
-            value: /^(?!\s+).+@.+(?<=\.[a-z]+)$/i,
-            message: 'Invalid email.'
-        }
-    },
-    password: {
-        minLength: {
-            value: 6,
-            message: 'Password should be at least 6 characters long.'
-        },
-        maxLength: {
-            value: 20,
-            message: 'Password should be 20 characters long at maximum.'
-        },
-        match: {
-            value: /^[A-Za-z0-9@$!%*#?&]{6,20}$/,
-            message: 'Password can include letters, digits or the following symbols @$!%*#?&'
-        }
-    }
-};
-
 const checks = {
     minLength(value, charCount) {
         return value.length >= charCount;
@@ -47,27 +10,39 @@ const checks = {
     }
 };
 
-export default function validator(fieldName, fieldValue) {
-    const fieldSchema = inputRequirementsSchema[fieldName];
-    
-    for (const condition in fieldSchema) {
-        if (!fieldSchema.hasOwnProperty(condition)) {
+const checkSchemaConditions = function (fieldName, fieldValue, schema) {
+    for (const check in schema[fieldName]) {
+        if (!schema[fieldName].hasOwnProperty(check)) {
             return;
         }
 
-        const { value: conditionValue, message } = fieldSchema[condition];
-        const valid = checks[condition](fieldValue, conditionValue);
+        const condition = schema[fieldName][check];
+        const isValid = checks[check](fieldValue, condition.value);
 
-        if (!valid) {
+        if (!isValid) {
             return {
-                valid,
-                message
+                name: fieldName,
+                message: condition.message,
+                valid: false
             };
         }
     }
 
     return {
-        valid: true,
-        message: ''
+        name: fieldName,
+        message: '',
+        valid: true
     }
+}
+
+export default function validator(formData, schema) {
+    const errors = [];
+
+    for (const fieldName in formData) {
+        const fieldValue = formData[fieldName];
+
+        errors.push(checkSchemaConditions(fieldName, fieldValue, schema));
+    }
+
+    return errors;
 }
