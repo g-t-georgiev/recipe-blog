8596714dbbd14dc414a337203bbf06d354129e91
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 00:55:24 +0200

    Simplified response processing and added request abortion on unmount.

--- src/components/auth/login/LoginForm.js
+++ src/components/auth/login/LoginForm.js
@@ -1,7 +1,9 @@
-import { useCallback, useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../../contexts/AuthContext';
+
 import * as authService from '../../../services/authService';
+import { validationSchema } from '../constants';
 
 import Form from '../../shared/form/Form';
 import FormInput from '../../shared/form-input/FormInput';
@@ -10,39 +12,41 @@ import FormFooter from '../../shared/form-footer/FormFooter';
 
 function LoginForm() {
     const { signIn } = useAuthContext();
-    const [ timerId, setTimer ] = useState(null);
+    const [ loginRequest, setLoginRequest] = useState(null);
+
+    useEffect(function () {
+        return function () {
+            loginRequest?.abort();
+        };
+    }, [loginRequest]);
+
     const redirectTo = useNavigate();
 
-    const redirectDelayHandler = useCallback(function () {
-        clearTimeout(timerId);
-        redirectTo('/');
-    }, [redirectTo, timerId]);
-
-    const loginHandler = useCallback(async function (formFieldData, updateFormLoadingStatus, updateFormSubmitStatus, updateFormFieldStatus, updateServiceResponseStatus) {
+    const login = useCallback(async function (formData, setFormStatus) {
         try {
-            updateFormLoadingStatus(true);
+            setFormStatus({ type: 'processing' });
     
-            const userData = await authService.login(formFieldData.email, formFieldData.password);
-            signIn(userData);
-            
-            updateServiceResponseStatus({ ok: true, message: ''});
+            const [userData, controller] = await authService.login(formData.email, formData.password);
 
-            setTimer(
-                setTimeout(redirectDelayHandler, 1500)
-            );
+            signIn(userData);
+
+            setLoginRequest(controller);
+
+            setFormStatus({ type: 'success' });
+
+            redirectTo('/');
         } catch (error) {
-            updateServiceResponseStatus({ ok: false, message: error.message });
-            Object.keys(formFieldData)
-                .forEach(field => updateFormFieldStatus(field, formFieldData[field], false, `Incorrect or non-existing ${field}.`));
-        } finally {
-            updateFormLoadingStatus(false);
-            updateFormSubmitStatus(true);
+            if (error.hasOwnProperty('multiple') || error.multiple === false) {
+                setFormStatus({ type: 'failed', payload: { message: error.message } });
+            } else {
+                setFormStatus({ type: 'failed', payload: { message: error.message.split(' ') } });
+            }
         }
-    }, [redirectDelayHandler, signIn]);
+    }, [redirectTo, signIn]);
 
     return (
-        <Form name="loginForm" title="Sign in to your account" submitAction={loginHandler}>
-            <FormInput type="email" name="email" id="email" placeholder="Email" />
+        <Form name="loginForm" title="Sign in to your account" schema={validationSchema} action={login}>
+            <FormInput type="text" name="email" id="email" placeholder="Email" />
             <FormInput type="password" name="password" id="password" placeholder="Password" />
             <FormButton text="Sign in" />
             <FormFooter span={{ text: 'Not a member yet?' }} link={{ text: 'Sign up.', to: '/users/register' }} />
--- src/components/auth/register/RegisterForm.js
+++ src/components/auth/register/RegisterForm.js
@@ -1,6 +1,8 @@
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import * as authService from '../../../services/authService';
+import { validationSchema } from '../constants';
 
 import Form from '../../shared/form/Form';
 import FormInput from '../../shared/form-input/FormInput';
@@ -8,37 +10,39 @@ import FormButton from '../../shared/form-button/FormButton';
 import FormFooter from '../../shared/form-footer/FormFooter';
 
 function RegisterForm() {
-    const [ timerId, setTimer ] = useState(null);
+    const [ registerRequest, setRegisterRequest ] = useState(null);
+
+    useEffect(function () {
+        return function () {
+            registerRequest?.abort();
+        };
+    }, [registerRequest]);
+
     const redirectTo = useNavigate();
 
-    const redirectDelayHandler = useCallback(function () {
-        clearTimeout(timerId);
-        redirectTo('/users/login');
-    }, [redirectTo, timerId]);
-
-    const registerHandler = useCallback(async function (formFieldData, updateFormLoadingStatus, updateFormSubmitStatus, updateFormFieldStatus, updateServiceResponseStatus) {
+    const register = useCallback(async function (formData, setFormStatus) {
         try {
-            updateFormLoadingStatus(true);
-            await authService.register(formFieldData.username, formFieldData.email, formFieldData.password);
-            updateServiceResponseStatus({ ok: true, message: ''});
+            setFormStatus({ type: 'processing' });
+            const [, controller] = await authService.register(formData.username, formData.email, formData.password);
 
-            setTimer(
-                setTimeout(redirectDelayHandler, 1500)
-            );
+            setFormStatus({ type: 'success' });
+
+            setRegisterRequest(controller);
+
+            redirectTo('/users/login')
         } catch (error) {
-            updateServiceResponseStatus({ ok: false, message: error.message });
-            Object.keys(formFieldData)
-                .forEach(field => updateFormFieldStatus(field, formFieldData[field], false, `Incorrect or non-existing ${field}.`));
-        } finally {
-            updateFormLoadingStatus(false);
-            updateFormSubmitStatus(true);
+            if (error.hasOwnProperty('multiple') || error.multiple === false) {
+                setFormStatus({ type: 'failed', payload: { message: error.message } });
+            } else {
+                setFormStatus({ type: 'failed', payload: { message: error.message.split(' ') } });
+            }
         }
-    }, [redirectDelayHandler]);
+    }, [redirectTo]);
 
     return (
-        <Form name="registerForm" title="Create new account" submitAction={registerHandler}>
+        <Form name="registerForm" title="Create new account" schema={validationSchema} action={register}>
             <FormInput type="text" name="username" id="username" placeholder="Username" />
-            <FormInput type="email" name="email" id="email" placeholder="Email" />
+            <FormInput type="text" name="email" id="email" placeholder="Email" />
             <FormInput type="password" name="password" id="password" placeholder="Password" />
             <FormButton text="Sign up" />
             <FormFooter span={{ text: 'Already have an account?' }} link={{ text: 'Sign in.', to: '/users/login' }} />
