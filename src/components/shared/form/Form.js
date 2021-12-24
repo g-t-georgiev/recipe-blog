bbae8d43adeb1632f355eb15cab011d6a7949fa4
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 00:50:07 +0200

    Added input validations on submit.

--- src/components/shared/form/Form.js
+++ src/components/shared/form/Form.js
@@ -1,10 +1,12 @@
 import { useCallback } from "react";
+import { useFormState } from "../../../hooks/useFormState";
+
 import './Form.css';
 
 import { FormContext } from "../../../contexts/FormContext";
-import { useFormState } from "../../../hooks/useFormState";
+import validator from './helpers/validateInput';
 
-function Form({ name, title = 'Fill in the form below', submitAction, children }) {
+function Form({ name, title = 'Fill in the form below', schema, action, children }) {
     const { formState, setFormState } = useFormState();
 
     const submitHandler = useCallback(function (e) {
@@ -12,18 +14,30 @@ function Form({ name, title = 'Fill in the form below', submitAction, children }
         e.stopPropagation();
 
         const form = e.currentTarget;
-        const formFieldData = Object.fromEntries(new FormData(form));
+        const formData = Object.fromEntries(new FormData(form));
 
-        submitAction.call(this, formFieldData, );
-    }, []);
+        if (validator(formData, schema)
+                .map(check => {
+                    setFormState({ type: 'validate', payload: check });
+                    return check;
+                })
+                .some(check => !check.valid)
+        ) return;
 
-    
+        action.call(form, formData, setFormState);
+    }, [setFormState, action, schema]);
 
     return (
-        <FormContext.Provider value={{ ...formState }}>
+        <FormContext.Provider value={formState}>
             <form className="form" name={name} autoComplete="off" onSubmit={submitHandler}>
                 <legend className="form-title">{title}</legend>
-                {(!formState.loading && formState.submitted && !formState.response.ok) && <span className="response-error">{formState.response.message}</span>}
+                {
+                    (!formState.loading && formState.submitted && formState.response) 
+                    && 
+                    Array.isArray(formState.response)
+                        ? formState.response.map((error, i) => <span key={i} className="response-error">{error}</span>)
+                        : <span className="response-error">{formState.response}</span>
+                }
                 {children}
             </form>
         </FormContext.Provider>
