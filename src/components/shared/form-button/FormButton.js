017665b1cb1225df26a810d68f09f6130c085d13
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 15:19:15 +0200

    Disable button if any input error is present in form state.

--- src/components/shared/form-button/FormButton.js
+++ src/components/shared/form-button/FormButton.js
@@ -2,14 +2,17 @@ import { useFormContext } from '../../../contexts/FormContext';
 import './FormButton.css';
 
 function FormButton({ type = 'submit', text = 'Submit' }) {
-    const formState = useFormContext();
+    const { 
+        formState,
+        checkForErrors
+    } = useFormContext();
 
     return (
         <div className="form-row">
             <button 
                 className="form-button" 
                 type={type} 
-                disabled={formState.loading}>
+                disabled={formState.loading || checkForErrors()}>
                 {formState.loading ? 'Loading...' : text}
             </button>
         </div>
