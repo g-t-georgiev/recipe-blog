55d21d9c0dbca074053a99bf04f089dbd8b1ad87
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 23:48:29 +0200

    Refactored validator function to return empty string if valid and message if invalid.

--- src/components/shared/form/helpers/validateInput.js
+++ src/components/shared/form/helpers/validateInput.js
@@ -1,52 +1,27 @@
 const checks = {
-    minLength(value, charCount) {
+    minLength: function (value, charCount) {
         return value.length >= charCount;
     },
-    maxLength(value, charCount) {
+    maxLength: function (value, charCount) {
         return value.length <= charCount;
     },
-    match(value, regex) {
+    match: function (value, regex) {
         return regex.test(value);
     }
 };
 
-const checkSchemaConditions = function (fieldName, fieldValue, schema) {
-    let result = [];
+export default function validator(fieldName, fieldValue, schema) {
+    let result = '';
 
     for (const check in schema[fieldName]) {
-        if (!schema[fieldName].hasOwnProperty(check)) {
+        const { value, message } = schema[fieldName][check];
+        const isValid = checks[check](fieldValue, value);
+
+        if (!isValid) {
+            result = message;
             break;
         }
-
-        const condition = schema[fieldName][check];
-        const isValid = checks[check](fieldValue, condition.value);
-
-        result.push({
-            name: fieldName,
-            message: isValid ? null : condition.message,
-            valid: isValid
-        });
     }
 
     return result;
-}
-
-export default function validator(fieldName, fieldValue, schema, formData = {}, checkOne = true) {
-    const errors = [];
-
-    for (fieldName in formData) {
-        if (checkOne) {
-            checkOne = false;
-        }
-
-        fieldValue = formData[fieldName];
-
-        errors.push(...checkSchemaConditions(fieldName, fieldValue, schema));
-    }
-
-    if (checkOne) {
-        errors.push(...checkSchemaConditions(fieldName, fieldValue, schema));
-    }
-
-    return errors;
 }
