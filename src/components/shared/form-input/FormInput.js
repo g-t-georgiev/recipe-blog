80ee48ff6a33f5df30bed573eeba51aa03438414
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 15:18:54 +0200

    Validate input inline and change form error stat if errors are present.

--- src/components/shared/form-input/FormInput.js
+++ src/components/shared/form-input/FormInput.js
@@ -1,26 +1,39 @@
+import { useCallback } from 'react';
 import { useFormContext } from '../../../contexts/FormContext';
 
 import './FormInput.css';
 
+import validator from '../form/helpers/validateInput';
+
 function FormInput(props) {
-    const formState = useFormContext();
+    const { 
+        schema,
+        checkForErrors,
+        updateFormState: {
+            updateFormErrorState
+        }
+    } = useFormContext();
+
+    const changeHandler = useCallback(function (e) {
+        let results = validator(props.name, e.currentTarget.value, schema);
+        updateFormErrorState(results);
+    }, [validator, updateFormErrorState]);
 
     return (
         <div className="form-row">
             <div className="input-field">
                 <label htmlFor={props.id}>
-                    <input className={`form-input ${formState.hasErrors(props.name) ? 'invalid' : ''}`} {...props} />
+                    <input className={`form-input ${checkForErrors(props.name) ? 'invalid' : ''}`} {...props} onChange={changeHandler} />
                     <span id="label-text">
                         {props.placeholder}
                     </span>
                 </label>
             </div>
             {
-                formState.hasErrors(props.name)
-                && formState.getErrors(props.name)
+                checkForErrors(props.name, true, true)
                     .map(
-                        (error) => (
-                            <span key={error.name + error.message} className="error-message">
+                        (error, i) => (
+                            <span key={error.name + i} className="error-message">
                                 {error.message}
                             </span>
                         )
