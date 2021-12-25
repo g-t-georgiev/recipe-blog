e4f9fad61db095ca4a511f7a52aa44b0004ba554
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 23:49:42 +0200

    Deleted validating input pattern and keeping track of validation errors from form state.

--- src/hooks/useFormState.js
+++ src/hooks/useFormState.js
@@ -2,47 +2,6 @@ import { useReducer, useCallback } from "react";
 
 const reducer = function (state, { type, payload }) {
     switch (type) {
-        case 'error': {
-            if (payload.valid) {
-                if (payload.error.name in state.errors) {
-                    let { [payload.error.name]: _, ...filteredErrors } = state.errors;
-                    return {
-                        ...state,
-                        errors: {
-                            ...filteredErrors
-                        }
-                    }
-                } else {
-                    return state;
-                }
-            } else {
-                if (payload.error.name in state.errors) {
-                    return state.errors[payload.error.name].includes(payload.error.message)
-                        ? state
-                        : {
-                            ...state,
-                            errors: {
-                                ...state.errors,
-                                [payload.error.name]: [
-                                    ...state.errors[payload.error.name],
-                                    payload.error.message
-                                ]
-                            }
-                        }
-                } else {
-                    return {
-                        ...state,
-                        errors: {
-                            ...state.errors,
-                            [payload.error.name]: [
-                                payload.error.message
-                            ]
-                        }
-                    };
-                }
-            }
-        }
-
         case 'processing': {
             return {
                 ...state,
@@ -75,44 +34,13 @@ const reducer = function (state, { type, payload }) {
 const initialState = {
     loading: false,
     submitted: false,
-    response: '',
-    errors: {}
+    response: ''
 };
 
 export function useFormState() {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const checkForErrors = useCallback(function (name = null, getDetails = false, multiple = false) {
-        if (name) {
-            return name in state.errors
-            && getDetails 
-            ? multiple
-            ? state.errors[name]
-            : state.errors[name][0] 
-            : name in state.errors;
-        }
-
-        for (const error in state.errors) {
-            return true;
-        }
-
-        return false;
-    }, [state]);
-
-    const updateFormErrorState = useCallback(function (payload, single = false) {
-        if (single) {
-            let { valid, ...error } = payload;
-            dispatch({ type: 'error', payload: { error, valid } });
-            return;
-        }
-
-        for (let item in payload) {
-            let { valid, ...error } = item;
-            dispatch({ type: 'error', payload: { error, valid } });
-        }
-    }, [dispatch]);
-
-    const updateFormLoadingState = useCallback(function (loading = true, ok = true, message = '', single = true) {
+    const updateFormState = useCallback(function (loading = true, ok = true, message = '', multiple = false) {
         if (loading) {
             dispatch({ type: 'processing' });
             return;
@@ -123,8 +51,8 @@ export function useFormState() {
             return;
         }
 
-        dispatch({ type: 'failed', payload: { message: single ? message : message.split(' ') }});
+        dispatch({ type: 'failed', payload: { message: multiple ? message.split(' ') : message }});
     }, [dispatch]);
 
-    return { formState: state, updateFormErrorState, updateFormLoadingState, checkForErrors };
+    return { formState: state, updateFormState };
 }
