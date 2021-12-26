6ef3a8cf0c2ff9ba0536e4e0a68841843265c3b8
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-26 14:37:19 +0200

    Used fetch hook and passed its return action object as a prop to form component.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,9 +1,33 @@
+// import { useCallback } from 'react';
 import LoginForm from "./LoginForm";
 
+import useFetch from '../../../hooks/useFetch';
+
+// const buttonStyles = {
+//     display: 'block',
+//     width: '23%',
+//     padding: '15px 10px',
+//     margin: '25px auto',
+//     borderColor: 'black',
+//     borderStyle: 'solid',
+//     borderWidth: '2px',
+//     backgroundColor: 'transparent',
+//     color: 'black',
+//     borderRadius: '5px',
+//     cursor: 'pointer'
+// }
+
 function Login() {
+    const action = useFetch('post', '/auth/login', true)
+
+    // const cancelLoginHandler = useCallback(function (e) {
+    //     abort();
+    // }, [abort]);
+
     return (
         <section>
-            <LoginForm />
+            <LoginForm action={action} />
+            {/* <button style={buttonStyles} onClick={cancelLoginHandler}>Cancel Request</button> */}
         </section>
     );
 }
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,9 +1,33 @@
+// import { useCallback } from 'react';
 import RegisterForm from './RegisterForm';
 
+import useFetch from '../../../hooks/useFetch';
+
+// const buttonStyles = {
+//     display: 'block',
+//     width: '23%',
+//     padding: '15px 10px',
+//     margin: '25px auto',
+//     borderColor: 'black',
+//     borderStyle: 'solid',
+//     borderWidth: '2px',
+//     backgroundColor: 'transparent',
+//     color: 'black',
+//     borderRadius: '5px',
+//     cursor: 'pointer'
+// }
+
 function Register() {
+    const action = useFetch('post', '/auth/register');
+
+    // const cancelLoginHandler = useCallback(function (e) {
+    //     abort();
+    // }, [abort]);
+
     return (
         <section>
-            <RegisterForm />
+            <RegisterForm action={action} />
+            {/* <button style={buttonStyles} onClick={cancelLoginHandler}>Cancel Request</button> */}
         </section>
     );
 }
