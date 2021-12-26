67b2482d69c5cf917d64125babb4a50eaa00e924
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-26 14:31:31 +0200

    Created default error page.

---
+++ src/components/error/ErrorPage/ErrorPage.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function ErrorPage(props) {
+    return (
+        <section>
+            <h2>Ooops, something went wrong...</h2>
+            <h3>We are terribly sorry. :(</h3>
+            <p>Try refreshing the page or navigate to <Link to="/">home</Link> page.</p>
+            <p>If you still are experiencing problems contact our support team.</p>
+        </section>
+    );
+}
+
+export default ErrorPage;
