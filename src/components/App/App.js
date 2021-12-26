c9fad33a1870e2939d9b240e8ebebd63402d3f53
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-26 14:33:55 +0200

    Wrapped all children components with an error boundary component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-import { AuthContextProvider } from '../../contexts/AuthContext';
-
 import './App.css';
+
+import ErrorBoundary from '../error/ErrorBoundary/ErrorBoundary';
+
+import { AuthContextProvider } from '../../contexts/AuthContext';
 
 import Header from '../header/Header';
 import Home from '../home/Home';
@@ -10,19 +12,21 @@ import Register from '../auth/register/Register';
 
 function App() {
 	return (
-		<AuthContextProvider>
-			<div className="layout-wrapper">
-				<Header />
-				<main className="main-content">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/recipes" element={<Home />} />
-						<Route path="/users/login" element={<Login />} />
-						<Route path="/users/register" element={<Register />} />
-					</Routes>
-				</main>
-			</div>
-		</AuthContextProvider>
+		<ErrorBoundary>
+			<AuthContextProvider>
+				<div className="layout-wrapper">
+					<Header />
+					<main className="main-content">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/recipes" element={<Home />} />
+							<Route path="/users/login" element={<Login />} />
+							<Route path="/users/register" element={<Register />} />
+						</Routes>
+					</main>
+				</div>
+			</AuthContextProvider>
+		</ErrorBoundary>
 	);
 }
 
