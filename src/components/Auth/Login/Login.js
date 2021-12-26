2bcd84a579c52e0462a8973887db40e25796ac26
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-26 17:41:27 +0200

    Refactored the use of fetch hook.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,33 +1,13 @@
-// import { useCallback } from 'react';
 import LoginForm from "./LoginForm";
 
 import useFetch from '../../../hooks/useFetch';
 
-// const buttonStyles = {
-//     display: 'block',
-//     width: '23%',
-//     padding: '15px 10px',
-//     margin: '25px auto',
-//     borderColor: 'black',
-//     borderStyle: 'solid',
-//     borderWidth: '2px',
-//     backgroundColor: 'transparent',
-//     color: 'black',
-//     borderRadius: '5px',
-//     cursor: 'pointer'
-// }
-
 function Login() {
-    const action = useFetch('post', '/auth/login', true)
-
-    // const cancelLoginHandler = useCallback(function (e) {
-    //     abort();
-    // }, [abort]);
+    const request = useFetch('post', '/auth/login', true);
 
     return (
         <section>
-            <LoginForm action={action} />
-            {/* <button style={buttonStyles} onClick={cancelLoginHandler}>Cancel Request</button> */}
+            <LoginForm action={request} />
         </section>
     );
 }
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,33 +1,13 @@
-// import { useCallback } from 'react';
 import RegisterForm from './RegisterForm';
 
 import useFetch from '../../../hooks/useFetch';
 
-// const buttonStyles = {
-//     display: 'block',
-//     width: '23%',
-//     padding: '15px 10px',
-//     margin: '25px auto',
-//     borderColor: 'black',
-//     borderStyle: 'solid',
-//     borderWidth: '2px',
-//     backgroundColor: 'transparent',
-//     color: 'black',
-//     borderRadius: '5px',
-//     cursor: 'pointer'
-// }
-
 function Register() {
-    const action = useFetch('post', '/auth/register');
-
-    // const cancelLoginHandler = useCallback(function (e) {
-    //     abort();
-    // }, [abort]);
+    const request = useFetch('post', '/auth/register');
 
     return (
         <section>
-            <RegisterForm action={action} />
-            {/* <button style={buttonStyles} onClick={cancelLoginHandler}>Cancel Request</button> */}
+            <RegisterForm action={request} />
         </section>
     );
 }
