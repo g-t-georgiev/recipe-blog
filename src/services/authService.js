5ff4794ac5aab926408615247f108114e038c643
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-27 20:31:12 +0200

    Added service methods for handling authentication requests.

---
+++ src/services/authService.js
@@ -0,0 +1,58 @@
+import getAccessToken from "./getAccessToken";
+import { authHeaderName, apiUrl } from "../constants";
+
+export const login = async function (email, password) {
+    const response = await fetch(apiUrl + '/auth/login', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    });
+    
+    if (!response.ok && response.status === 404) {
+        throw new Error('Connection error.');
+    } else if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+    }
+
+    return response.json();
+}
+
+export const register = async function (username, email, password) {
+    const response = await fetch(apiUrl + '/auth/register', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, email, password })
+    });
+
+    if (!response.ok && response.status === 404) {
+        throw new Error('Connection error.');
+    } else if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+    }
+
+    return null;
+}
+
+export const logout = async function () {
+    const response = await fetch(apiUrl + '/auth/logout', {
+        method: 'delete',
+        headers: {
+            [authHeaderName]: `Bearer ${getAccessToken()}`
+        }
+    });
+
+    if (!response.ok && response.status === 404) {
+        throw new Error('Connection error.');
+    } else if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+    }
+
+    return null;
+}
