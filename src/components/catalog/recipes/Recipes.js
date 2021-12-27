ec42a9a81625eb193d57aac6b57393f6a9fa82dc
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-27 23:57:53 +0200

    Created main catalog component for recipes.

---
+++ src/components/catalog/recipes/Recipes.js
@@ -0,0 +1,10 @@
+function Recipes() {
+    return (
+        <section>
+            <h2>All Recipes</h2>
+            <p>This is a collection of all recipes.</p>
+        </section>
+    );
+}
+
+export default Recipes;
