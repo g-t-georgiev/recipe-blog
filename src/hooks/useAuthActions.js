aaa5a83c4db3c9118aa327d1a4a55c6578b63442
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-27 20:32:51 +0200

    Added custom hook for binding authentication service actions, authentication context updates and redirects on successful completion.

---
+++ src/hooks/useAuthActions.js
@@ -0,0 +1,30 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../contexts/AuthContext'
+import * as authService from '../services/authService';
+
+function useAuthActions() {
+    const { signIn, signOut } = useAuthContext();
+    const redirectTo = useNavigate();
+
+    const login = useCallback(async function ({ email, password }) {
+        const userData = await authService.login(email, password);
+        signIn(userData);
+        return () => redirectTo('/');
+    }, [signIn, redirectTo]);
+
+    const register = useCallback(async function ({ username, email, password }) {
+        await authService.register(username, email, password);
+        return () => redirectTo('/users/login');
+    }, [redirectTo]);
+
+    const logout = useCallback(async function () {
+        await authService.logout();
+        signOut();
+        return () => redirectTo('/');
+    }, [signOut, redirectTo]);
+
+    return { login, register, logout };
+}
+
+export default useAuthActions;
