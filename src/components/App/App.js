6aa80a97f802155c9d99cf1d7f7ad8d3a5b05482
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-27 23:59:03 +0200

    Added recipes catalog to routes.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,7 @@ import Header from '../header/Header';
 import Home from '../home/Home';
 import Login from '../auth/login/Login';
 import Register from '../auth/register/Register';
+import Recipes from '../catalog/recipes/Recipes';
 
 function App() {
 	return (
@@ -19,7 +20,7 @@ function App() {
 					<main className="main-content">
 						<Routes>
 							<Route path="/" element={<Home />} />
-							<Route path="/recipes" element={<Home />} />
+							<Route path="/recipes" element={<Recipes />} />
 							<Route path="/users/login" element={<Login />} />
 							<Route path="/users/register" element={<Register />} />
 						</Routes>
