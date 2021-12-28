c221b9241b97fb916a3e8f057107d2c6a045268f
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-28 12:10:42 +0200

    Changed recipe data passing prop name.

--- src/components/catalog/recipes/RecipeCard/RecipeCard.js
+++ src/components/catalog/recipes/RecipeCard/RecipeCard.js
@@ -1,14 +1,14 @@
-function RecipeCard({ recipe }) {
+function RecipeCard({ data }) {
     return (
         <div className="recipe-card">
             <div className="recipe-card-image">
-                <img src={recipe.imageUrl} alt={recipe.title} />
+                <img src={data.imageUrl} alt={data.title} />
             </div>
             <div className="recipe-card-info">
-                <h4 className="recipe-card-info-title">{recipe.title}</h4>
-                <p className="recipe-card-info-category">{recipe.category}</p>
-                <p className="recipe-card-info-autho">{recipe.author.username}</p>
-                <p>Rating: {reviewCount && recipe.rating.reduce((rating, review) => rating += review.rating, 0) / recipe.reviewCount}</p>
+                <h4 className="recipe-card-info-title">{data.title}</h4>
+                <p className="recipe-card-info-category">{data.category}</p>
+                <p className="recipe-card-info-autho">{data.author.username}</p>
+                <p>Rating: {data.reviewCount && data.rating.reduce((rating, review) => rating += review.rating, 0) / data.reviewCount}</p>
                 <button className="recipe-card-details-button">Details</button>
             </div>
         </div>
