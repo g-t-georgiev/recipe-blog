2be0c2b8928565fce3ae8fc45c78db24b8c7deae
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-28 13:14:47 +0200

    Added local constants for recipe with initial entry validation schema for create and edit forms.

---
+++ src/components/catalog/recipes/constants.js
@@ -0,0 +1,33 @@
+export const validationSchema = {
+    title: {
+        minLength: {
+            value: 5,
+            message: 'Title should be at least 5 characters long.'
+        },
+        maxLength: {
+            value: 50,
+            message: 'Title should be 50 characters long at maximum.'
+        }
+    },
+    description: {
+        minLength: {
+            value: 15,
+            message: 'Description should be at least 15 characters long.'
+        },
+        maxLength: {
+            value: 3000,
+            message: 'Desciprion should be 3000 characters long at maximum.'
+        }
+    },
+    category: {
+        required: {
+            message: 'Choose a category from drop down list.'
+        }
+    },
+    imageUrl: {
+        match: {
+            value: /^https?:\/\//i,
+            message: 'Invalid image URL.'
+        }
+    }
+};
