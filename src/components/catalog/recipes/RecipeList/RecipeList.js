11d2248ddea44a992ad0a89814aaeeca89682a09
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-28 10:54:11 +0200

    Added base for more complex API query and refactored rendered JSX  container structure.

--- src/components/catalog/recipes/RecipeList/RecipeList.js
+++ src/components/catalog/recipes/RecipeList/RecipeList.js
@@ -4,40 +4,47 @@ import { Link, useSearchParams } from 'react-router-dom';
 
 import useFetch from '../../../../hooks/useFetch';
 
-function RecipeList() {
+function RecipeList(props) {
     const [ searchParams, setSearchParams ] = useSearchParams();
     const path = useMemo(function () {
-        return searchParams.has('category')
-        ? `?category=${searchParams.get('category')}`
-        : '';
-    }, [searchParams]);
+        let category = (props.category || searchParams.get('category')) ?? '';
+        // let page = (props.page || searchParams.get('page')) ?? '1';
+        // let show = (props.show || searchParams.get('show')) ?? '5';
+
+        let path = category && `?category=${category}`;
+        return path;
+    }, [props.category, searchParams]);
 
     const recipes = useFetch('/data/recipes' + path);
 
-    if (['idle', 'fetching'].includes(recipes.status)) {
-        return (
-            <section>
-                <h3>Loading...</h3>
-            </section>
-        );
-    }
-
     return (
-        <>
+        <section className="recipe-list">
             {
                 recipes.status === 'fetched'
                 ? Array.isArray(recipes.data) && recipes.data.length > 0
-                ? <ul>{recipes.data.map(recipe => <li key={recipe._id}>{recipe.title}</li>)}</ul>
-                : <section>
-                    <h3>No recipes to show</h3>
-                    <p>Start <Link to="/recipes/create">adding</Link> some of yours.</p>
-                </section>
-                : <section>
-                    <h3>Oops, something went wrong while getting recipes data.</h3>
-                    <p>Issue: {recipes.error}</p>
-                </section>
+                ? recipes.data.map(recipe => <li key={recipe._id}>{recipe.title}</li>)
+                : (
+                    <>
+                        <h3>No recipes to show</h3>
+                        <p>Contribute by <Link to="/recipes/create">adding</Link> some of yours.</p>
+                    </>
+                )
+                : ['idle', 'fetching'].includes(recipes.status)
+                ? (
+                    <>
+                        <h3>Loading...</h3>
+                        <p>Please, wait. We are fetching some data for you.</p>
+                    </>
+                )
+                : (
+                    <>
+                        <h3>Oops, something went wrong while getting recipes data.</h3>
+                        <p>Issue: {recipes.error}</p>
+                    </>
+                )
+
             }
-        </>
+        </section>
     );
 }
 
