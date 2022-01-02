12127df2a7ca15706996f2b79a1bbc556a17e3ef
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-01-03 00:02:48 +0200

    Created 404 page.

---
+++ src/components/404/404.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <>
+            <h2>Nothing found</h2>
+            <p>The page you are looking for doesn't exist.</p>
+        </>
+    );
+}
+
+export default NotFound;
