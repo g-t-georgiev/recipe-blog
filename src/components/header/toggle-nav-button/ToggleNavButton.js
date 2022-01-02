72a118bb3a9cbc7554290082d30020e623134c51
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-01-02 22:15:48 +0200

    Removed old header, app and home directories.

--- src/components/header/toggle-nav-button/ToggleNavButton.js
+++
@@ -1,7 +0,0 @@
-function ToggleNavButton({ openNavHandler }) {
-    return (
-        <button onClick={openNavHandler} className="toggle-navigation-button">&#9776;</button>
-    )
-}
-
-export default ToggleNavButton;
