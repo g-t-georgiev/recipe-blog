435fe92b366e946f2678947f3b7565d9f14a5164
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-01-02 21:51:52 +0200

    Changed import paths and restructured route nesting  hierarchy.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,91 +1,101 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 
-import ErrorBoundary from '../error/ErrorBoundary/ErrorBoundary';
-
+import ErrorBoundary from '../error/error-boundary/ErrorBoundary';
 import { AuthContextProvider } from '../../contexts/AuthContext';
-
+import { RecipeContextProvider } from '../../contexts/RecipeContext';
+import Layout from '../shared/layout/Layout';
+import Content from '../shared/content/Content';
+import Section from '../shared/section/Section';
 import Header from '../header/Header';
 import Home from '../home/Home';
 import Login from '../auth/login/Login';
 import Register from '../auth/register/Register';
 import Recipes from '../catalog/recipes/Recipes';
-import RecipeList from '../catalog/recipes/RecipeList/RecipeList';
-import CreateRecipeForm from '../catalog/recipes/CreateRecipeForm/CreateRecipeForm';
-import RecipeDetails from '../catalog/recipes/RecipeDetails/RecipeDetails';
-import EditRecipeForm from '../catalog/recipes/EditRecipeForm/EditRecipeForm';
-import DeleteRecipeForm from '../catalog/recipes/DeleteRecipeForm/DeleteRecipeForm';
-import MyRecipesList from '../catalog/recipes/MyRecipesList/MyRecipesList';
-import Favorites from '../catalog/recipes/Favorites/Favorites';
-import ErrorPage from '../error/ErrorPage/ErrorPage';
+import RecipeList from '../catalog/recipes/recipe-list/RecipeList';
+import CreateRecipeForm from '../catalog/recipes/create-recipe-form/CreateRecipeForm';
+import RecipeDetails from '../catalog/recipes/recipe-details/RecipeDetails';
+import EditRecipeForm from '../catalog/recipes/edit-recipe-form/EditRecipeForm';
+import DeleteRecipeForm from '../catalog/recipes/delete-recipe-form/DeleteRecipeForm';
+import MyRecipesList from '../catalog/recipes/my-recipes-list/MyRecipesList';
+import Favorites from '../catalog/recipes/favorites/Favorites';
+import ErrorPage from '../error/error-page/ErrorPage';
 import RouteGuard from '../shared/route-guard/RouteGuard';
 
 function App() {
 	return (
 		<ErrorBoundary>
 			<AuthContextProvider>
-				<div className="layout-wrapper">
+				<Layout>
 					<Header />
-					<main className="main-content">
+					<Content>
 						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="recipes" element={<Recipes />}>
-								<Route index element={<RecipeList />} />
-								<Route path=":recipeId">
-									<Route index element={<RecipeDetails />} />
+							<Route path="/" element={<Section />}>
+								<Route index element={<Home />} />
+								<Route path="recipes" element={<Recipes />}>
+									<Route index element={<RecipeList />} />
+
+									<Route path=":recipeId" element={<RecipeContextProvider />}>
+										<Route index element={<RecipeDetails />} />
+										<Route
+											path="edit"
+											element={
+												<RouteGuard isPrivate={true}>
+													<EditRecipeForm />
+												</RouteGuard>
+											} />
+										<Route
+											path="delete"
+											element={
+												<RouteGuard isPrivate={true}>
+													<DeleteRecipeForm />
+												</RouteGuard>
+											} />
+									</Route>
+
 									<Route
-										path="edit"
+										path="create"
 										element={
 											<RouteGuard isPrivate={true}>
-												<EditRecipeForm />
-											</RouteGuard>
-										} />
-									<Route
-										path="delete"
-										element={
-											<RouteGuard isPrivate={true}>
-												<DeleteRecipeForm />
+												<CreateRecipeForm />
 											</RouteGuard>
 										} />
 								</Route>
-								<Route
-									path="create"
-									element={
-										<RouteGuard isPrivate={true}>
-											<CreateRecipeForm />
+
+								<Route path="users">
+									<Route path="login" element={
+										<RouteGuard isPrivate={false}>
+											<Login />
 										</RouteGuard>
 									} />
-							</Route>
-							<Route path="users">
-								<Route path="login" element={
-									<RouteGuard isPrivate={false}>
-										<Login />
-									</RouteGuard>
-								} />
-								<Route path="register" element={
-									<RouteGuard isPrivate={false}>
-										<Register />
-									</RouteGuard>
-								} />
-								<Route path=":userId">
-									<Route path="recipes" element={
-										<RouteGuard isPrivate={true}>
-											<MyRecipesList />
+
+									<Route path="register" element={
+										<RouteGuard isPrivate={false}>
+											<Register />
 										</RouteGuard>
 									} />
-									<Route path="favorites" element={
-										<RouteGuard isPrivate={true}>
-											<Favorites />
-										</RouteGuard>
-									} />
+
+									<Route path=":userId">
+										<Route path="recipes" element={
+											<RouteGuard isPrivate={true}>
+												<MyRecipesList />
+											</RouteGuard>
+										} />
+
+										<Route path="favorites" element={
+											<RouteGuard isPrivate={true}>
+												<Favorites />
+											</RouteGuard>
+										} />
+									</Route>
 								</Route>
+
+								<Route path="*" element={<ErrorPage />} />
 							</Route>
-							<Route path="*" element={<ErrorPage />} />
 						</Routes>
-					</main>
-				</div>
+					</Content>
+				</Layout>
 			</AuthContextProvider>
-		</ErrorBoundary>
+		</ErrorBoundary >
 	);
 }
 
